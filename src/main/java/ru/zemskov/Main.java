4b55a1622586f37afa323a7aec5c6002481e54d0
konstantinzemskov <1500konstantin@gmail.com>
2024-03-05 11:10:01 +0300

    Initial commit

---
+++ src/main/java/ru/zemskov/Main.java
@@ -0,0 +1,7 @@
+package ru.zemakov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+    }
+}
---
+++ src/main/java/ru/zemskov/StringParser.java
@@ -0,0 +1,2 @@
+package ru.zemskov;public class StringParser {
+}
