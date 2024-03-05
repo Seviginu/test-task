6ca8b4ec36f92113af2d338a217c986bcbd46e22
seviginu <1500konstantin@gmail.com>
2024-03-06 00:06:04 +0300

    little refactor

--- src/main/java/ru/zemskov/Main.java
+++ src/main/java/ru/zemskov/Main.java
@@ -3,38 +3,58 @@ package ru.zemskov;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        long startTime = System.nanoTime();
+    public static void printGroups(List<List<List<String>>> groups) {
+        System.out.printf("Количество групп: %d\n", groups.size());
+        int groupCounter = 0;
+        for (var group : groups) {
+            System.out.printf("ГРУППА %d\n", ++groupCounter);
+            for (var row : group) {
+                int elCounter = 0;
+                for (var el : row) {
+                    System.out.print(el);
+                    if(++elCounter != row.size())
+                        System.out.print(";");
+                }
+                System.out.println();
+            }
+        }
+    }
 
-        String filename = args.length == 1 ? args[0] : "lng.txt";
+    private static void fillGrouper(StringGrouper grouper, StringParser parser) throws IOException {
+        while (parser.hasNextLine()) {
+            grouper.addLine(parser.parseNextLine());
+        }
+    }
 
+    public static void stringGroup(String filename) {
         try (FileInputStream fis = new FileInputStream(filename)) {
             StringParser parser = new StringParser(fis);
             StringGrouper grouper = new StringGrouper();
-            while (parser.hasNextLine()) {
-                grouper.addLine(parser.parseNextLine());
-            }
+            fillGrouper(grouper, parser);
             var groups = grouper.group();
-            System.out.printf("Количество групп: %d\n", groups.size());
-            int groupCounter = 0;
-            for (var group : groups) {
-                if (group.size() <= 1) continue;
-                System.out.printf("ГРУППА %d\n", ++groupCounter);
-                for (var row : group) {
-                    for (var el : row) {
-                        System.out.print(el);
-                        System.out.print(";");
-                    }
-                    System.out.println();
-                }
-            }
-            System.out.printf("Время работы программы: %d ms\n", (System.nanoTime() - startTime) / 1000000);
+
+            printGroups(groups);
+
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IOException e) {
-            System.out.println("Ошибка чтения");
+            System.out.println("Ошибка чтения файла");
         }
+    }
+
+
+
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Необходимо указать файл(java -jar app.jar *filename*.txt");
+            return;
+        }
+        String filename = args[0];
+        long startTime = System.nanoTime();
+        stringGroup(filename);
+        System.out.printf("Время работы программы: %d", (System.nanoTime()-startTime)/1000000);
     }
 }
