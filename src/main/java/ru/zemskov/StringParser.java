982f56b74ea6028f7edb600aff50f857b502863a
seviginu <1500konstantin@gmail.com>
2024-03-07 13:18:22 +0300

    Optimize

--- src/main/java/ru/zemskov/StringParser.java
+++ src/main/java/ru/zemskov/StringParser.java
@@ -13,27 +13,13 @@ public class StringParser {
         this.strings = strings;
     }
 
-    private List<String> split(String string, char split) {
-        int ptr = 0;
-        List<String> list = new ArrayList<>();
-        StringBuilder builder = new StringBuilder();
-        while (ptr < string.length()) {
-            if (string.charAt(ptr) == split) {
-                list.add(builder.toString());
-                builder.setLength(0);
-            } else {
-                builder.append(string.charAt(ptr));
-            }
-            ptr++;
-        }
-        list.add(builder.toString());
-        return list;
-    }
+
 
     public Set<List<String>> parseFile() throws IOException {
         Set<List<String>> parsedLines = new HashSet<>();
         for (String a : strings) {
-            parsedLines.add(split(a, ';'));
+            String[] splitArray = a.split(";");
+            parsedLines.add(List.of(splitArray));
         }
         return parsedLines;
     }
